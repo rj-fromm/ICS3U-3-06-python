87d36d8eab7b2d077482b7c5c9557850da68fef8
Ubuntu <ubuntu@ip-172-31-25-71.ec2.internal>
2019-10-08 18:28:38 +0000

    updated guessing program

---
+++ updated-guessing.py
@@ -0,0 +1,28 @@
+#!user/bin/env python3
+
+# Created by: RJ Fromm
+# Created on: September 2019
+# This program is a number guessing game
+
+import random
+
+
+def main():
+
+
+    try:
+        random_number = random.randint(1, 100+1)
+
+        secret_number = int(input("Guess my secret number : "))
+
+        if secret_number == random_number:
+            print("WOW! YOU GUESSED THE RANDOM NUMBER! WHAT DO YOU WANT? A MEDAL")
+
+        else:
+            print("The random number was", (random_number))
+
+    except Exception:
+        print("Enter a real number")
+
+if __name__ == "__main__":
+    main()
